6efc743df737dd224eb0f3d3ec177a9617804937
glaschenko <a.glaschenko@gmail.com>
2023-06-05 18:46:01 +0400

    Initial commit

---
+++ src/main/java/com/ag/petclinicAG/PetclinicAgApplication.java
@@ -0,0 +1,13 @@
+package com.ag.petclinicAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicAgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicAgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ag/petclinicAG/PetclinicAgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.petclinicAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicAgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
